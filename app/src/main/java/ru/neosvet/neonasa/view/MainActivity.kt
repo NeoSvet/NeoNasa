0c47f4f16afa8eb9f002eb7730b9d67e022fa0e8
Neosvet <neosvet333@gmail.com>
2021-06-03 10:57:29 +0300

    realized load photo of day with description

--- app/src/main/java/ru/neosvet/neonasa/view/MainActivity.kt
+++ app/src/main/java/ru/neosvet/neonasa/view/MainActivity.kt
@@ -1,11 +1,16 @@
-package ru.neosvet.neonasa
+package ru.neosvet.neonasa.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.neosvet.neonasa.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, DayPhotoFragment())
+            .commit()
     }
 }
