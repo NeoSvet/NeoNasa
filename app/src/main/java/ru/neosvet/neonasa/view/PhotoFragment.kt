1694a7d0d5525acc4addb7f708a752af73b7ad21
Neosvet <neosvet333@gmail.com>
2021-06-16 15:31:59 +0300

    add scroll photo

--- app/src/main/java/ru/neosvet/neonasa/view/PhotoFragment.kt
+++ app/src/main/java/ru/neosvet/neonasa/view/PhotoFragment.kt
@@ -1,13 +1,17 @@
 package ru.neosvet.neonasa.view
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.util.Size
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import android.webkit.WebView
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.animation.addListener
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -49,6 +53,16 @@ class PhotoFragment : Fragment(), Observer<PhotoState>, Callback {
     private lateinit var tvInfo: TextView
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
     private var isExpanded = false
+    private val animPhotoRestore: ObjectAnimator by lazy {
+        ObjectAnimator.ofFloat(ivPhoto, "translationX", 0f)
+            .apply {
+                duration = 500
+            }
+    }
+    private val sizeScreen: Size by lazy {
+        val screen: View = mainAct.window.findViewById(Window.ID_ANDROID_CONTENT)
+        Size(screen.width, screen.height)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -114,8 +128,45 @@ class PhotoFragment : Fragment(), Observer<PhotoState>, Callback {
         )
 
         ivPhoto.scaleType =
-            if (isExpanded) ImageView.ScaleType.CENTER_CROP
+            if (isExpanded) ImageView.ScaleType.FIT_XY
             else ImageView.ScaleType.FIT_START
+
+
+        val params = ivPhoto.getLayoutParams() as ViewGroup.MarginLayoutParams
+
+        if (isExpanded) {
+            val img_h = ivPhoto.drawable.intrinsicHeight
+            val img_w = ivPhoto.drawable.intrinsicWidth
+
+            params.height = sizeScreen.height
+            val f: Float = img_w.toFloat() * (sizeScreen.height.toFloat() / img_h.toFloat())
+            params.width = f.toInt()
+
+            animPhotoScroll(f)
+        } else {
+            params.height = ViewGroup.LayoutParams.MATCH_PARENT
+            params.width = ViewGroup.LayoutParams.MATCH_PARENT
+            animPhotoRestore.start()
+        }
+        ivPhoto.setLayoutParams(params)
+    }
+
+    private fun animPhotoScroll(photoWidth: Float) {
+        val time = (photoWidth / sizeScreen.width * 1100).toLong()
+        val toEnd = -(photoWidth - sizeScreen.width)
+        val anim1 = ObjectAnimator.ofFloat(ivPhoto, "translationX", toEnd)
+            .apply {
+                duration = time
+            }
+        anim1.addListener(onEnd = {
+            val toCenter = toEnd * 0.543478f
+            val anim2 = ObjectAnimator.ofFloat(ivPhoto, "translationX", toCenter)
+                .apply {
+                    duration = time / 2
+                }
+            anim2.start()
+        })
+        anim1.start()
     }
 
     private fun setBottomSheet() {
